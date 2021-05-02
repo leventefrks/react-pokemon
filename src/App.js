f23eedae6f21255d11e76604c99d649288686481
x-farkasle <x-farkasle@wizzair.com>
2021-05-02 23:51:18 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
     <div className="App bg-gray-100 p-6">
       <nav className="flex justify-between capitalize text-2xl text-center mb-4 font-bold">
         <button
-          className={`px-4 py-2 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`px-4 py-2 text-sm font-semibold text-indigo-700 transition-all ease-linear duration-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             prevUrl ? 'opacity-100' : 'opacity-0'
           }`}
           onClick={onClickPrevious}
@@ -44,7 +44,7 @@ function App() {
         </button>
         <h1 className="self-center text-3xl">Pokemon</h1>
         <button
-          className={`px-4 py-2 text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`px-4 py-2 text-sm font-semibold text-indigo-700 transition-all ease-linear duration-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             nextUrl ? 'opacity-100' : 'opacity-0'
           }`}
           onClick={onClickNext}

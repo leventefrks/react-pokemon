bde820fccaf963bec7ffe16a002081d3db825c33
x-farkasle <x-farkasle@wizzair.com>
2021-05-03 08:18:35 +0200

    pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from './axios';
 import { Loader } from './components/Loader';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import PokemonList from './components/PokemonList';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         setLoading(false);
       } catch (error) {
         console.log(error);
+        throw error;
       }
     };
 
@@ -35,21 +37,23 @@ function App() {
     <div className="App bg-gray-100 p-6">
       <nav className="flex justify-between capitalize text-2xl text-center mb-4 font-bold">
         <button
-          className={`px-4 py-2 text-sm font-semibold text-indigo-700 transition-all ease-linear duration-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`flex items-center px-4 py-2 text-sm font-semibold text-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             prevUrl ? 'opacity-100' : 'opacity-0'
           }`}
           onClick={onClickPrevious}
         >
+          <FaChevronLeft className="w-3 h-3 mr-2 fill-current" />
           Previous
         </button>
         <h1 className="self-center text-3xl">Pokemon</h1>
         <button
-          className={`px-4 py-2 text-sm font-semibold text-indigo-700 transition-all ease-linear duration-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`flex items-center px-4 py-2 text-sm font-semibold text-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             nextUrl ? 'opacity-100' : 'opacity-0'
           }`}
           onClick={onClickNext}
         >
           Next
+          <FaChevronRight className="w-3 h-3 ml-2 fill-current" />
         </button>
       </nav>
       <div className="min-h-screen space-y-4 md:space-y-0 md:grid gap-10 md:grid-cols-3 lg:grid-cols-4 grid-flow-row">

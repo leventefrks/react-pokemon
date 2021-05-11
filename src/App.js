500b2b613a59b9ff89ecd6abefc50e6729f5729f
x-farkasle <x-farkasle@wizzair.com>
2021-05-11 22:00:52 +0200

    basic search

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [prevUrl, setPrevUrl] = useState(null);
   const [selectedItem, setSelectedItem] = useState(null);
   const [isModalVisible, setModalVisibility] = useState(false);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const source = axios.CancelToken.source();
@@ -77,6 +78,30 @@ function App() {
 
   const onClickPrevious = () => setUrl(prevUrl);
 
+  const onFetchData = async () => {
+    if (!query) return;
+    setLoading(true);
+    const response = await axios(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
+
+    const { id, name, weight, height, types } = response.data;
+    const image = response.data.sprites.front_default;
+
+    const singleItem = {
+      id,
+      name,
+      weight,
+      height,
+      image,
+      types,
+    };
+
+    console.log(response);
+    setPokemonList([singleItem]);
+    setLoading(false);
+  };
+
   return (
     <div
       className={`bg-gray-100 p-4 md:p-6 w-full min-h-screen ${
@@ -85,7 +110,7 @@ function App() {
     >
       <nav className="flex justify-between capitalize text-2xl text-center mb-10">
         <button
-          className={`flex items-center justify-between px-2 md:px-4 md:py-2 text-sm font-semibold text-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`flex items-center justify-between px-2 md:px-4 md:py-2 text-sm font-semibold text-purple-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             prevUrl ? 'block' : 'hidden'
           }`}
           onClick={onClickPrevious}
@@ -95,7 +120,7 @@ function App() {
         </button>
         <AppTitle />
         <button
-          className={`flex items-center px-2 md:px-4 md:py-2 text-sm font-semibold text-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
+          className={`flex items-center px-2 md:px-4 md:py-2 text-sm font-semibold text-purple-700 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ${
             nextUrl ? 'block' : 'hidden'
           }`}
           onClick={onClickNext}
@@ -104,7 +129,30 @@ function App() {
           <FaChevronRight className="w-3 h-3 ml-2 fill-current" />
         </button>
       </nav>
-      <div className="min-h-screen max-w-5xl mx-auto space-y-4 md:space-y-0 grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 grid-flow-row">
+      <div className="w-full flex justify-center mb-12">
+        <label
+          htmlFor="query"
+          className="block mb-2 text-sm text-gray-600"
+        ></label>
+        <div className="flex items-center justify-center space-x-3">
+          <input
+            type="text"
+            name="query"
+            id="query"
+            placeholder="Find your Pokemon"
+            className="search-input"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+          <button
+            className="bg-purple-500 hover:bg-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-2 text-white rounded-md"
+            onClick={onFetchData}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="min-h-screen max-w-5xl mx-auto space-y-4 md:space-y-0 grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 auto-rows-auto">
         <PokemonList
           pokemonList={pokemonList}
           onSelected={onSelected}
--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -1,6 +1,6 @@
 export const AppTitle = () => {
   return (
-    <h1 className="mx-auto text-lg md:text-3xl lg:md-text-4xl font-bold text-indigo-600">
+    <h1 className="absolute top-6 -inset-x-1/2 translate text-lg md:text-3xl lg:md-text-4xl font-bold text-indigo-600">
       Get your Pokemon
     </h1>
   );
